7a29cccd40d3d198b4034c0cecca1f6c1ec921e6
Robert Beeger <robert@beeger.net>
2018-08-26 21:19:06 +0200

    Add animation

--- Fortune/Fortune.swift
+++ Fortune/Fortune.swift
@@ -10,44 +10,59 @@ import Foundation
 import ScreenSaver
 
 class Fortune: ScreenSaverView {
-    private let textDisplay: NSTextField
+    private var textDisplays: [NSTextField]
+    private var xConstraints: [NSLayoutConstraint]
+    private var current: Int
 
     override init?(frame: NSRect, isPreview: Bool) {
-        textDisplay = NSTextField()
+        textDisplays = [NSTextField(), NSTextField()]
+        xConstraints = []
+        current = 0
         super.init(frame: frame, isPreview: isPreview)
-        
+
+        wantsLayer = true
         animationTimeInterval = 60
-        
+
         let box = NSBox()
         box.boxType = .custom
         box.fillColor = NSColor(red:0.118, green:0.29, blue: 0.365, alpha:  1)
-        
+
         box.translatesAutoresizingMaskIntoConstraints = false
         addSubview(box)
         box.topAnchor.constraint(equalTo: topAnchor).isActive = true
         box.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
         box.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
         box.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
-        
-        addSubview(textDisplay)
+
+        addSubview(textDisplays[0])
+        addSubview(textDisplays[1])
+        configureTextDisplay(textDisplay: textDisplays[0], backgroundColor: box.fillColor)
+        configureTextDisplay(textDisplay: textDisplays[1], backgroundColor: box.fillColor)
+        textDisplays[1].alphaValue = 0.0
+        xConstraints.append(textDisplays[0].centerXAnchor.constraint(equalTo: self.centerXAnchor))
+        xConstraints[0].isActive = true
+        xConstraints.append(textDisplays[1].centerXAnchor.constraint(equalTo: self.centerXAnchor, constant: -600))
+        xConstraints[1].isActive = true
+    }
+    
+    required init?(coder decoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func configureTextDisplay(textDisplay: NSTextField, backgroundColor: NSColor) {
         textDisplay.translatesAutoresizingMaskIntoConstraints = false
         textDisplay.isEditable = false
         textDisplay.isSelectable = false
         textDisplay.textColor = NSColor(red:0.97, green:0.96, blue:0.88, alpha:1.00)
-        textDisplay.backgroundColor = box.fillColor
+        textDisplay.backgroundColor = NSColor.clear
         textDisplay.drawsBackground = true
         textDisplay.isBordered = false
         textDisplay.isBezeled = false
         textDisplay.maximumNumberOfLines = 40
         textDisplay.font = NSFont(name: "Hoefler Text", size: 25)
 
-        textDisplay.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
         textDisplay.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
         textDisplay.widthAnchor.constraint(equalToConstant: 1000).isActive = true
-    }
-    
-    required init?(coder decoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
     
     override func animateOneFrame() {
@@ -56,10 +71,24 @@ class Fortune: ScreenSaverView {
         let pipe = Pipe()
         task.standardOutput = pipe
         task.launch()
-        
+
         let data = pipe.fileHandleForReading.readDataToEndOfFile()
         let output = String(data: data, encoding: String.Encoding.utf8)!
-        textDisplay.attributedStringValue = NSAttributedString(string: output)
+        let next = 1 - current
+        textDisplays[next].attributedStringValue = NSAttributedString(string: output)
+
+        NSAnimationContext.runAnimationGroup({ context in
+            context.duration = 5
+            context.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+            context.allowsImplicitAnimation = true
+            textDisplays[next].alphaValue = 1.0
+            textDisplays[current].alphaValue = 0.0
+            xConstraints[next].constant = 0.0
+            xConstraints[current].constant = current == 0 ? 600 : -600
+            layoutSubtreeIfNeeded()
+        }, completionHandler: {
+            self.current = next
+        })
 
         super.animateOneFrame()
     }
