7fa1cf1714b83bb0d6a2b4a177b662778509ca1f
Robert Beeger <robert@beeger.net>
2020-05-21 12:16:51 +0200

    Add app and tests
    Cannot add tests for screensaver directly. Need to have an app hosting the tests.

---
+++ EpigraverAppTests/EpigraverAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  EpigraverAppTests.swift
+//  EpigraverAppTests
+//
+//  Created by Robert Beeger on 21.05.20.
+//  Copyright © 2020 Robert Beeger. All rights reserved.
+//
+
+import XCTest
+@testable import EpigraverApp
+
+class EpigraverAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
